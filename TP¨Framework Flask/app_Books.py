ccc652e149af5dd1beacc8e98757551d562fb683
BaptisteHurel <58144828+BaptisteHurel@users.noreply.github.com>
2020-11-30 11:48:47 +0100

    import debut tp

---
+++ TP¨Framework Flask/app_Books.py
@@ -0,0 +1,37 @@
+from flask import Flask
+app = Flask(__name__)
+
+from flask import render_template
+
+
+import json
+
+#url = http://127.0.0.1:5000/
+
+#route principale
+@app.route("/")
+def index():
+ return "hello my app"
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
+#variable book
+book=[
+	{
+		'id':1,
+		'titre' : 'un titre',
+	},
+	{
+		'id':2,
+		'titre': 'un autre titre random',
+	}
+]
+    
+#url = http://127.0.0.1:5000/api/books   
+@app.route('/api/books', methods=["GET"])
+def index():
+    response = get_a_response()
+    return book
+
+
