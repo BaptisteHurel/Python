3956c5e0213e81928fc037d5b30afa9820e7d206
BaptisteHurel <58144828+BaptisteHurel@users.noreply.github.com>
2020-12-08 11:57:23 +0100

    up

--- Projet API ML/app Fast API/main.py
+++ Projet API ML/app Fast API/main.py
@@ -24,4 +24,4 @@ def read_item(item_id: int, q: Optional[str] = None):
 
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
-    return {"item_name": item.name, "item_id": item_id}
+    return {"item_name": item.name, "item_price": item.price}
