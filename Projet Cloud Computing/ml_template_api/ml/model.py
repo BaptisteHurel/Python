d54d1288604e2ce8a58b52b20003310a034215f4
BaptisteHurel <58144828+BaptisteHurel@users.noreply.github.com>
2021-01-06 11:44:15 +0100

    update

---
+++ Projet Cloud Computing/ml_template_api/ml/model.py
@@ -0,0 +1,7 @@
+from utils.utils import DataHandler, FeatureRecipe, FeatureExtractor
+
+def DataManager(d:DataHandler=None, fr: FeatureRecipe=None, fe:FeatureExtractor=None):
+    """
+        Fonction qui lie les 3 premières classes de la pipeline et qui fait un return FeatureExtractor.split(0.1)
+    """
+    pass
