2b4f5e49fb7fe6a864a82edecf91d4dbae638d28
Baptiste Hurel <58144828+BaptisteHurel@users.noreply.github.com>
2021-10-28 10:07:38 +0200

    Delete Cloud Computing Project TDD/fastapi-tdd-docker/app directory

--- Cloud Computing Project TDD/fastapi-tdd-docker/app/models/pydantic.py
+++
@@ -1,9 +0,0 @@
-# project/app/models/pydantic.py
-
-from pydantic import BaseModel
-
-class SummaryPayloadSchema(BaseModel):
-    url: str
-
-class SummaryResponseSchema(SummaryPayloadSchema):
-    id: int
